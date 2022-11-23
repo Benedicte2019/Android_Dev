f1fafac1e0c96ce1244363985847517383e0f393
aanuorioke <oriokeaanuoluwapo@yahoo.com>
2022-11-23 07:29:43 +0200

    changed addservice icon

--- SukaFasta/app/src/main/java/com/example/sukafasta/screen/BottomNavItems.kt
+++ SukaFasta/app/src/main/java/com/example/sukafasta/screen/BottomNavItems.kt
@@ -1,10 +1,7 @@
 package com.example.sukafasta.screen
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.DateRange
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.*
 
 object BottomNavItems {
 
@@ -21,7 +18,7 @@ object BottomNavItems {
         ),
         BottomBarItem(
             title = "Add Service",
-            image = Icons.Filled.Settings,
+            image = Icons.Filled.AddCircle,
             route = "add_service"
         ),
         BottomBarItem(
