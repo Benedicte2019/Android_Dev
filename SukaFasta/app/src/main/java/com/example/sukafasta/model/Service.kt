39aafccab07fd1d9ea95f44631a9fc82faf64417
Furaha Benedict <benefatia@gmail.com>
2022-12-12 13:47:42 +0200

    added service data class

--- SukaFasta/app/src/main/java/com/example/sukafasta/model/Service.kt
+++ SukaFasta/app/src/main/java/com/example/sukafasta/model/Service.kt
@@ -1,4 +1,6 @@
 package com.example.sukafasta.model
 
-class Service {
+import androidx.compose.ui.graphics.vector.ImageVector
+
+class Service (val id: String, var servName: String, var serviceDesc: String, var img: ImageVector) {
 }
