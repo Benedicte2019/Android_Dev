d909a7f4a266528f26b199cb11c07f91967ddc2f
macBrig <mugwuchu@andrew.cmu.edu>
2022-12-13 01:39:37 +0200

    updating addservice
    updated

--- SukaFasta/app/src/main/java/com/example/sukafasta/screen/AddProduct.kt
+++ SukaFasta/app/src/main/java/com/example/sukafasta/screen/AddProduct.kt
@@ -1,5 +1,6 @@
 package com.example.sukafasta.screen
 
+<<<<<<< Updated upstream
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
@@ -48,10 +49,39 @@ fun MainScreen1() {
         addProduct()
 //        addService()
     }
+=======
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.media.Image
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.sukafasta.ui.theme.SukaFastaTheme
+
+@Composable
+fun AddProduct(){
+>>>>>>> Stashed changes
 
 }
 
 @Composable
+<<<<<<< Updated upstream
 fun BottomBar1() {
     val selectedItem = remember { mutableStateOf(0) }
     BottomNavigation(
@@ -225,3 +255,60 @@ fun addProduct() {
 
 
 
+=======
+fun PickImageFromGallery() {
+
+    var imageUri by remember { mutableStateOf<Uri?>(null) }
+    val context = LocalContext.current
+    val bitmap = remember { mutableStateOf<Bitmap?>(null) }
+
+    val launcher =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri: Uri? ->
+            imageUri = uri
+        }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        imageUri?.let {
+            if (Build.VERSION.SDK_INT < 28) {
+                bitmap.value = MediaStore.Images
+                    .Media.getBitmap(context.contentResolver, it)
+            } else {
+                val source = ImageDecoder.createSource(context.contentResolver, it)
+                bitmap.value = ImageDecoder.decodeBitmap(source)
+            }
+
+            bitmap.value?.let { btm ->
+                Image(
+                    bitmap = btm.asImageBitmap(),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(400.dp)
+                        .padding(20.dp)
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Button(onClick = { launcher.launch("image/*") }) {
+            Text(text = "Pick Image")
+        }
+    }
+
+}
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview(){
+    SukaFastaTheme() {
+        AddService {
+
+        }
+
+    }
+}
+>>>>>>> Stashed changes
--- SukaFasta/app/src/main/java/com/example/sukafasta/screen/AddService.kt
+++ SukaFasta/app/src/main/java/com/example/sukafasta/screen/AddService.kt
@@ -2,6 +2,7 @@ package com.example.sukafasta.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+<<<<<<< Updated upstream
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -16,6 +17,16 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+=======
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+>>>>>>> Stashed changes
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -26,6 +37,7 @@ import androidx.compose.ui.unit.sp
 import com.example.sukafasta.ui.theme.SukaFastaTheme
 
 @Composable
+<<<<<<< Updated upstream
 fun MainScreen() {
     Scaffold(
         topBar = {
@@ -215,3 +227,213 @@ fun DefaultPreview() {
         MainScreen()
     }
 }
+=======
+fun AddService(function: () -> Unit) {
+        Column(
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .height(height = 65.dp)
+                .padding(top = 575.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .height(height = 65.dp)
+                    .background(color = Color.White))
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .width(width = 303.dp)
+                    .height(height = 43.dp)
+                    .padding(
+                        start = 28.dp,
+                        top = 584.dp
+                    )
+            ) {
+                Column(
+                    verticalArrangement = Arrangement.Bottom,
+                    modifier = Modifier
+                        .width(width = 40.dp)
+                        .height(height = 43.dp)
+                        .padding(top = 584.dp)
+                ) {
+//                    Image(
+//                        painter = painterResource(id = R.drawable.material-symbols:home-rounded),
+//                        contentDescription = "material-symbols:home-rounded",
+//                        colorFilter = ColorFilter.tint(Color(0xff514545)),
+//                        modifier = Modifier
+//                            .size(size = 28.dp))
+                    Text(
+                        text = "Home",
+                        color = Color(0xff514545),
+                        textAlign = TextAlign.Center,
+                        style = androidx.compose.ui.text.TextStyle(
+                            fontSize = 14.sp
+                        )
+                    )
+                }
+                Spacer(
+                    modifier = Modifier
+                        .width(width = 70.dp))
+                Column(
+                    verticalArrangement = Arrangement.Bottom,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .width(width = 78.dp)
+                        .height(height = 41.dp)
+                        .padding(top = 586.dp)
+                ) {
+//                    Image(
+//                        painter = painterResource(id = R.drawable.bi:calendar-check),
+//                        contentDescription = "bi:calendar-check",
+//                        colorFilter = ColorFilter.tint(Color(0xff514545)),
+//                        modifier = Modifier
+//                            .size(size = 24.dp))
+                    Text(
+                        text = "My Booking",
+                        color = Color(0xff514545),
+                        textAlign = TextAlign.Center,
+                        style = androidx.compose.ui.text.TextStyle(
+                            fontSize = 14.sp
+                        )
+                    )
+                }
+                Spacer(
+                    modifier = Modifier
+                        .width(width = 60.dp))
+                Column(
+                    verticalArrangement = Arrangement.Bottom,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .width(width = 55.dp)
+                        .height(height = 43.dp)
+                        .padding(top = 584.dp)
+                ) {
+//                    Image(
+//                        painter = painterResource(id = R.drawable.mdi-light:account),
+//                        contentDescription = "mdi-light:account",
+//                        colorFilter = ColorFilter.tint(Color(0xff18978f)),
+//                        modifier = Modifier
+//                            .size(size = 28.dp))
+                    Text(
+                        text = "Account",
+                        color = Color(0xff18978f),
+                        textAlign = TextAlign.Center,
+                        style = androidx.compose.ui.text.TextStyle(
+                            fontSize = 14.sp
+                        )
+                    )
+                }
+            }
+        }
+        Surface(
+            shape = RoundedCornerShape(2f),
+            modifier = Modifier
+                .width(width = 328.dp)
+                .height(height = 32.dp)
+                .padding(
+                    start = 12.dp,
+                    top = 19.dp
+                )
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.End
+            ) {
+//                Image(
+//                    painter = painterResource(id = R.drawable.material-symbols:arrow-back-rounded),
+//                    contentDescription = "material-symbols:arrow-back-rounded",
+//                    colorFilter = ColorFilter.tint(Color(0xff152c47)),
+//                    modifier = Modifier
+//                        .width(width = 30.dp)
+//                        .height(height = 24.dp))
+                Spacer(
+                    modifier = Modifier
+                        .width(width = 13.dp))
+                Text(
+                    text = "Add Service",
+                    color = Color.Black,
+                    style = androidx.compose.ui.text.TextStyle(
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Bold
+                    ),
+                    modifier = Modifier
+                        .width(width = 285.dp)
+                        .height(height = 32.dp))
+            }
+        }
+        Box(
+            modifier = Modifier
+                .width(width = 328.dp)
+                .height(height = 50.dp)
+                .background(color = Color.White))
+        Text(
+            text = buildAnnotatedString {
+                withStyle(style = SpanStyle(
+                    color = Color.Black,
+                    fontSize = 14.sp)
+                ) {append("Add Service name")}
+                withStyle(style = SpanStyle(
+                    color = Color(0xfff35b5b),
+                    fontSize = 14.sp)) {append("*")}})
+        Box(
+            modifier = Modifier
+                .width(width = 328.dp)
+                .height(height = 70.dp)
+                .background(color = Color.White))
+        Box(
+            modifier = Modifier
+                .width(width = 328.dp)
+                .height(height = 70.dp)
+                .background(color = Color.White))
+        Text(
+            text = "Add Service description",
+            color = Color.Black,
+            style = androidx.compose.ui.text.TextStyle(
+                fontSize = 14.sp
+            )
+        )
+        Text(
+            text = "Upload cover picture",
+            color = Color.Black,
+            style = androidx.compose.ui.text.TextStyle(
+                fontSize = 14.sp
+            ),
+            modifier = Modifier
+                .width(width = 165.dp))
+        Column(
+            verticalArrangement = Arrangement.Bottom,
+            modifier = Modifier
+                .width(width = 328.dp)
+                .height(height = 45.dp)
+                .padding(
+                    start = 17.dp,
+                    top = 494.dp
+                )
+        ) {
+            Box(
+                modifier = Modifier
+                    .width(width = 328.dp)
+                    .height(height = 45.dp)
+                    .background(color = Color(0xff18978f)))
+            Text(
+                text = "Add New Service",
+                color = Color.White,
+                textAlign = TextAlign.Center,
+                style = androidx.compose.ui.text.TextStyle(
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Bold
+                ),
+                modifier = Modifier
+                    .width(width = 328.dp)
+                    .height(height = 43.dp))
+
+        }
+//        Image(
+//            painter = painterResource(id = R.drawable.ic:baseline-image-search),
+//            contentDescription = "ic:baseline-image-search",
+//            colorFilter = ColorFilter.tint(Color(0xffd9d9d9)),
+//            modifier = Modifier
+//                .size(size = 50.dp))
+
+}
+>>>>>>> Stashed changes
