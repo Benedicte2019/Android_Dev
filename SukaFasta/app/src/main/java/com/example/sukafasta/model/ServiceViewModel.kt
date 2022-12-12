99040bcc606d5de70dad16554a167e1ca3554c5b
Furaha Benedict <benefatia@gmail.com>
2022-12-12 13:48:49 +0200

    added service view model

--- SukaFasta/app/src/main/java/com/example/sukafasta/model/ServiceViewModel.kt
+++ SukaFasta/app/src/main/java/com/example/sukafasta/model/ServiceViewModel.kt
@@ -1,4 +1,16 @@
 package com.example.sukafasta.model
 
-class ServiceViewModel {
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.ViewModel
+
+class ServiceViewModel: ViewModel() {
+    var servicesList = mutableStateListOf<Service>()
+
+    fun addService(newService: Service){
+        servicesList.add(newService)
+    }
+
+    fun delete(service: Service){
+        servicesList.remove(service)
+    }
 }
