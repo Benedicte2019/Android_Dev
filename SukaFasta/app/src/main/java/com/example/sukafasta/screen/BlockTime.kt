a6b23c628d65312ffab6179ccdf6fd3c29949ea1
Furaha Benedict <benefatia@gmail.com>
2022-12-12 18:28:22 +0200

    added block time screen

---
+++ SukaFasta/app/src/main/java/com/example/sukafasta/screen/BlockTime.kt
@@ -0,0 +1,10 @@
+package com.example.sukafasta.screen
+
+import androidx.compose.runtime.Composable
+
+//class BlockTime {
+//}
+
+// allows hairdresser to block time
+//@Composable
+//fun BlockTime(){}
